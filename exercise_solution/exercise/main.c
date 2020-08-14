2ce98e5062f35a46c9ac400c3368149b624c53bf
sohee <totns6543@naver.com>
2020-08-14 19:57:27 +0900

    비밀번호 입력후 비교

--- exercise_solution/exercise/main.c
+++ exercise_solution/exercise/main.c
@@ -8,16 +8,16 @@
 
 char KeyScan();
 void check_PW();
+void keyInput();
 
 char KeyBuf[30];
 char buf[10];
 char inputPassword [10];
 char match [10]="1234";
+char key;
 
 int main()
 {
-	char key;
-
 	clcd_port_init();
 	clcd_init_8bit();
 	
@@ -34,21 +34,41 @@ int main()
 		}
 		else
 		{
-			//clcd_init_8bit();
-			//sprintf(buf,"%c", key);
-			//clcd_str(buf);
-			for (int i = 0; i< 4; i++)
-			{
-				buf[i] = inputPassword[i];
-			}
-			//clcd_str(buf);
+			keyInput();
 		}
-		check_PW();
+	}
+}
+
+void keyInput()
+{
+	clcd_init_8bit();
+	int i = 0;
+	while(1)
+	{
+		key=KeyScan();
+		if(key != 0xFF)
+		{
+			clcd_position(0,i);
+			sprintf(buf, "%c", key);
+			clcd_str(buf);
+			inputPassword[i] = key;
+			i++;
+			if(strcmp(buf,"*")==0)
+			{
+				check_PW();
+			}
+			else if(strcmp(buf,"0")==0)
+			{
+				char buf[10]="";
+				main();
+			}
+		}
 	}
 }
 
 void check_PW()
 {
+	clcd_init_8bit();
 	int isMatch = 1;
 	for (int j = 0; j<4; j++)
 	{
@@ -76,28 +96,28 @@ char KeyScan()
 	DDRE=0x0F;         // 비트0,1,2,3 출력으로 지정
 
 	PORTE&=~1; // 1번째 줄 선택
-	_delay_ms(3);
+	_delay_ms(2);
 	if((PINE&0x10)==0)KeyBuf='1';
 	if((PINE&0x20)==0)KeyBuf='2';
 	if((PINE&0x40)==0)KeyBuf='3';
 	PORTE|=1; // 1번째 줄 해제
 
 	PORTE&=~2; // 2번째 줄 선택
-	_delay_ms(3);
+	_delay_ms(2);
 	if((PINE&0x10)==0)KeyBuf='4';
 	if((PINE&0x20)==0)KeyBuf='5';
 	if((PINE&0x40)==0)KeyBuf='6';
 	PORTE|=2; // 2번째 줄 해제
 
 	PORTE&=~4; // 3번째 줄 선택
-	_delay_ms(3);
+	_delay_ms(2);
 	if((PINE&0x10)==0)KeyBuf='7';
 	if((PINE&0x20)==0)KeyBuf='8';
 	if((PINE&0x40)==0)KeyBuf='9';
 	PORTE|=4; // 3번째 줄 해제
 
 	PORTE&=~8; // 4번째 줄 선택
-	_delay_ms(3);
+	_delay_ms(2);
 	if((PINE&0x10)==0)KeyBuf='*';
 	if((PINE&0x20)==0)KeyBuf='0';
 	if((PINE&0x40)==0)KeyBuf='#';
