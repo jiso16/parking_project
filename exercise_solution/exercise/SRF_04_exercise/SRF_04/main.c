83dd3a6051f6c8aa25d62066b3957617c22092d0
sohee <totns6543@naver.com>
2020-08-28 16:09:16 +0900

    초음파센서랑 lcd 같이 동작시켜보기, 초음파센서 여러개 동작 안 함

--- exercise_solution/exercise/SRF_04_exercise/SRF_04/main.c
+++ exercise_solution/exercise/SRF_04_exercise/SRF_04/main.c
@@ -2,36 +2,88 @@
 #include <stdio.h>
 #define F_CPU 16000000UL  // 16 MHz
 #include <util/delay.h>
+#include "clcd_D8.h"
 //
-#define Trigger_1       PORTC|=1  //초음파 트리거
-#define Trigger_0       PORTC&=~1 //초음파 트리거
-#define Echo            (PINC&2)  //초음파 에코
+#define Trigger_1       PORTD|=1  //초음파 트리거
+#define Trigger_0       PORTD&=~1 //초음파 트리거
+#define Echo            (PIND&2)  //초음파 에코
 //
 unsigned int range;
 //
-void getEcho(void){
+void getEcho(void)
+{
 	while(!Echo);            // high가 될때까지 대기 //
 	TCNT1=0; TCCR1B=2;       // 카운터 시작, 8분주 = 0.5us //
 	while(Echo);             // low가 될때까지 대기 //
 	TCCR1B=8;                // 카운터 정지 //
 	range=TCNT1/116;         // Cm로 변경 //
 }
-int main(void){
-	DDRC=1;                  // PC0 ouput Trigger, PC1 input Echo
+int main(void)
+{
+	clcd_port_init();//CPU초기화
+	clcd_init_8bit();//LCD초기화
+	
+	DDRD=1;                  // PD0 ouput Trigger, PC1 input Echo
 	DDRB=0xFF;               // LED out
 	TCCR1A=0; TCCR1B=8;      // CTC mode
-	while(1){
+	while(1)
+	{
 		Trigger_1; _delay_us(10); Trigger_0;    // 10uS
 		getEcho();
-		if     (range< 5){ PORTB=0b00000000; }
-		else if(range<10){ PORTB=0b00000001; }
-		else if(range<15){ PORTB=0b00000011; }
-		else if(range<20){ PORTB=0b00000111; }
-		else if(range<25){ PORTB=0b00001111; }
-		else if(range<30){ PORTB=0b00011111; }
-		else if(range<35){ PORTB=0b00100000; }
-		else if(range<40){ PORTB=0b01000000; }
-		else             { PORTB=0b10000000; }
-		_delay_ms(50);
+		if     (range< 5)
+		{ 
+			clcd_str("range < 50cm");
+			_delay_ms(100);
+			clcd_init_8bit();
+		}
+		else if(range<10)
+		{
+			clcd_str("range < 1m");
+			 _delay_ms(100);
+			clcd_init_8bit();
+		 }
+		else if(range<15)
+		{
+			 clcd_str("range < 1.5m");
+			 _delay_ms(100);
+			 clcd_init_8bit();
+	    }
+		else if(range<20)
+		{
+			 clcd_str("range < 2m");
+			 _delay_ms(100);
+			 clcd_init_8bit();
+		 }
+		else if(range<25)
+		{
+			 clcd_str("range < 2.5m");
+			 _delay_ms(100);
+			 clcd_init_8bit();
+		 }
+		else if(range<30)
+		{
+			 clcd_str("range < 3m");
+			 _delay_ms(100);
+			 clcd_init_8bit();
+		 }
+		else if(range<35)
+		{
+			 clcd_str("range < 3.5m");
+			 _delay_ms(100);
+			 clcd_init_8bit();
+		 }
+		else if(range<40)
+		{
+			 clcd_str("range < 4m");
+			 _delay_ms(100);
+			 clcd_init_8bit();
+		 }
+		else             
+		{ 
+			clcd_str("range < 4.5m");
+			_delay_ms(100);
+			clcd_init_8bit();
+		}
+		_delay_ms(100);
 	}
 }
--- exercise_solution/exercise/multi_sr04_solution/multi_sr04/main.c
+++ exercise_solution/exercise/multi_sr04_solution/multi_sr04/main.c
@@ -1,30 +1,32 @@
+#define F_CPU 16000000UL 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
-
+#include "clcd_D8.h"
 
 
 //
-#define Trig1_ON PORTB|=1 //트리거1
-#define Trig2_ON PORTB|=2 //트리거2
-#define Trig3_ON PORTB|=4 //트리거3
+#define Trig1_ON PORTD|=1 //트리거1
+#define Trig2_ON PORTD|=2 //트리거2
+#define Trig3_ON PORTD|=4 //트리거3
 //#define Trig4_ON PORTB|=8 //트리거4
 //
-#define Trig1_OFF PORTB&=~1 //트리거1
-#define Trig2_OFF PORTB&=~2 //트리거2
-#define Trig3_OFF PORTB&=~4 //트리거3
+#define Trig1_OFF PORTD&=~1 //트리거1
+#define Trig2_OFF PORTD&=~2 //트리거2
+#define Trig3_OFF PORTD&=~4 //트리거3
 //#define Trig4_OFF PORTB&=~8 //트리거4
 //
 char s[30];
-unsigned int range_I, range[4];
+unsigned int range_I, range[3];
 float range_F;
 //
 char temp[3]={'0','0','0'};
 //
-void getEcho(int ch){ // ch=0~3
-	while(!(PINB&(0x40<<ch)));  // Wait for echo pin to go high
+void getEcho(int ch)
+{ // ch=0~3
+	while(!(PORTD&(0x40<<ch)));  // Wait for echo pin to go high
 	TCNT1=0x00; TCCR1B=0x02;    // 1:8 prescaler = 0.5us
-	while(PINB&(0x40<<ch));     // Wait for echo pin to go low
+	while(PORTD&(0x40<<ch));     // Wait for echo pin to go low
 	TCCR1B=0x08; range_I=TCNT1; // the range in CM
 	range_F=(float)range_I;
 	range_F/=11.6; // mm
@@ -34,27 +36,34 @@ void getEcho(int ch){ // ch=0~3
 
 
 //
-int main(void){
-	DDRA = 0xc0;
-	DDRC = 0xff;
+int main(void)
+{
 
-
-	DDRB=0x0F;  // PB0~3 ouput Trigger, PB4~7 input Echo
+	//int i  = 0;
+	clcd_port_init();//CPU초기화
+	clcd_init_8bit();//LCD초기화
+	
+	DDRD=0x0F;  // PB0~3 ouput Trigger, PB4~7 input Echo
 
 
 	TCCR1B=0x08;  // Set timer up in CTC mode
 	_delay_ms(100);
 	//    init(); // lcd 초기화 함수
-	while(1){
-		_delay_ms(25); Trig1_ON; _delay_ms(10); Trig1_OFF; getEcho(0); PORTA = 0B00000001;
-		_delay_ms(25); Trig2_ON; _delay_ms(10); Trig2_OFF; getEcho(1); PORTA = 0B00000010;
-		_delay_ms(25); Trig3_ON; _delay_ms(10); Trig3_OFF; getEcho(2); PORTA = 0B00000100;
+	while(1)
+	{
+		_delay_ms(25); Trig1_ON; _delay_ms(10); Trig1_OFF; getEcho(0); clcd_position(0,1); clcd_str("1"); _delay_ms(100); clcd_init_8bit();
+		_delay_ms(25); Trig2_ON; _delay_ms(10); Trig2_OFF; getEcho(1); clcd_position(0,3); clcd_str("2"); _delay_ms(100); clcd_init_8bit();
+		_delay_ms(25); Trig3_ON; _delay_ms(10); Trig3_OFF; getEcho(2); clcd_position(0,5); clcd_str("3"); _delay_ms(100); clcd_init_8bit();
 		//_delay_ms(25); Trig4_ON; _delay_us(10); Trig4_OFF; getEcho(3);
 		//
-		
+		 //sprintf(temp,"1:%3d 2:%3d 3:%3d",range[0],range[1],range[2]);
 		//
 		//        lcd_inst(0x01);  _delay_ms(10);
-		//        lcd_strout(0x00+i,temp[i]); // lcd 문자열 함수 이름으로 바꾸세요
+		//clcd_str(temp); // lcd 문자열 함수 이름으로 바꾸세요
+		//i++;
+		//_delay_ms(50);
+		//clcd_init_8bit();//LCD초기화
+
 	}
 }
 
