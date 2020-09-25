8133d6ee0e142739650473f96444e7213440b11e
sohee <totns6543@naver.com>
2020-09-25 14:47:52 +0900

    delay 바꿈

--- parking_solution/parking_Solution2/main.c
+++ parking_solution/parking_Solution2/main.c
@@ -297,7 +297,7 @@ void Warning()
 {
 	currentPage = OPEN;
 	clcd_str("Wrong Number!");
-	_delay_ms(3000);
+	_delay_ms(1000);
 	clcd_init_8bit();
 }
 
@@ -483,28 +483,28 @@ char KeyScan()
 	DDRE=0x0F;         // 비트0,1,2,3 출력으로 지정
 
 	PORTE&=~1; // 1번째 줄 선택
-	_delay_ms(30);
+	_delay_ms(28);
 	if((PINE&0x10)==0)keybuf='1';
 	if((PINE&0x20)==0)keybuf='2';
 	if((PINE&0x40)==0)keybuf='3';
 	PORTE|=1; // 1번째 줄 해제
 
 	PORTE&=~2; // 2번째 줄 선택
-	_delay_ms(30);
+	_delay_ms(28);
 	if((PINE&0x10)==0)keybuf='4';
 	if((PINE&0x20)==0)keybuf='5';
 	if((PINE&0x40)==0)keybuf='6';
 	PORTE|=2; // 2번째 줄 해제
 
 	PORTE&=~4; // 3번째 줄 선택
-	_delay_ms(30);
+	_delay_ms(28);
 	if((PINE&0x10)==0)keybuf='7';
 	if((PINE&0x20)==0)keybuf='8';
 	if((PINE&0x40)==0)keybuf='9';
 	PORTE|=4; // 3번째 줄 해제
 
 	PORTE&=~8; // 4번째 줄 선택
-	_delay_ms(30);
+	_delay_ms(28);
 	if((PINE&0x10)==0)keybuf='*';
 	if((PINE&0x20)==0)keybuf='0';
 	if((PINE&0x40)==0)keybuf='#';
