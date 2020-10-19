86c3a8827a0c9213151f38187a8b8a264490c173
sohee <totns6543@naver.com>
2020-10-19 19:23:11 +0900

    주차장 파일분할

---
+++ exercise_solution/exercise/lcd_whiles_solution/lcd_while/function.c
@@ -0,0 +1,174 @@
+﻿#include "parking.h"
+
+//비밀번호, 키 관련 변수
+char key;
+char keybuf[30];
+char buf[10];
+char input_password [4];
+char inital_password [4];
+char area_password [3][5];
+int i ;
+int k ;
+int m ;
+int num ;
+int num2 ;
+
+volatile int x;
+
+void InitUserMode()
+{
+	clcd_init_8bit();
+	clcd_str("Visitor mode.");
+	clcd_position(1, 0);
+	clcd_str("Enter Your Area");
+}
+
+void AreaNum()
+{
+	clcd_init_8bit();
+	clcd_str("Area ");
+	clcd_str(buf);
+	clcd_position(1,0);
+	clcd_str("New PW:");
+}
+void AreaNum2()
+{
+	clcd_init_8bit();
+	clcd_str("Area ");
+	clcd_str(buf);
+}
+
+void PwInput() // 관리자모드 비번 입력
+{
+	clcd_init_8bit();
+
+	clcd_str("Enter the Admin");
+	clcd_position(1,0);
+	clcd_str("password:");
+}
+
+void CheckAdminPW() // 관리자 모드 비번 확인
+{
+	clcd_init_8bit();
+	int isMatch = 1;
+	for (int j = 0; j<4; j++)
+	{
+		if(input_password[j] != inital_password[j])
+		{
+			isMatch = 0;
+		}
+	}
+	if(isMatch == 1)
+	{
+		//i = 9;
+		currentPage = Admin_PW_OK;
+	}
+	else
+	{
+		clcd_str("Wrong PW");
+		currentPage = OPEN2;
+	}
+	
+}
+void UserPwInput() // 유저 비번 확인
+{
+	clcd_init_8bit();
+	clcd_str("Enter the PW");
+}
+
+void CheckUserPW(int num2) // 유저 모드 비번 확인
+{
+	clcd_init_8bit();
+	int isMatch = 1;
+	for (int j = 0; j<4; j++)
+	{
+		if(input_password[j] != area_password[num2][j])
+		{
+			isMatch = 0;
+		}
+	}
+	if(isMatch == 1)
+	{
+		clcd_init_8bit();
+		clcd_str("Have a nice day");
+		
+		currentPage = Gate_Open;
+	}
+	else
+	{
+		clcd_str("Wrong PW");
+		currentPage = OPEN2;
+	}
+	
+}
+
+void GateOpen()
+{
+	DDRB=0x20;   // PB5 out
+	TCCR1A=0x82; TCCR1B=0x1A; OCR1A=3000; ICR1=19999; // OCR1A -> OC Clear / Fast PWM TOP = ICR1 / 8ºÐ??
+	
+	OCR1A = 1900; // 0 degree
+	
+	currentPage = Gate_Close;
+}
+
+void GateClose()
+{
+	DDRB=0x20;   // PB5 out
+	TCCR1A=0x82; TCCR1B=0x1A; OCR1A=3000; ICR1=19999; // OCR1A -> OC Clear / Fast PWM TOP = ICR1 / 8ºÐ??
+
+	OCR1A = 700; // 90 degree
+	
+	
+	currentPage = OPEN2;
+}
+
+
+
+void Init()
+{
+	clcd_init_8bit();
+	clcd_str("mode ");
+	clcd_str(buf);
+}
+
+void Open()
+{
+	clcd_init_8bit();
+	clcd_str("Select the mode");
+	
+}
+
+void Open2()
+{
+	clcd_init_8bit();
+	clcd_str("1) Admin mode");
+	clcd_position(1, 0);
+	clcd_str("2) Visitor mode");
+}
+
+void Warning()
+{
+	clcd_init_8bit();
+	clcd_str("Wrong Number!");
+	currentPage = OPEN;
+}
+void print_Str()
+{
+	
+	clcd_position(0, 0);
+	clcd_str("Parking System");
+	
+}
+
+void io_setting()
+{
+	DDRA = 0xFF;
+}
+
+void timer()
+{
+	TCCR0 = ((1<<CS02)|(0<<CS01)|(0<<CS00));
+	TCNT0 = -250;
+	TIMSK = ((1<<TOIE0));
+}
---
+++ exercise_solution/exercise/lcd_whiles_solution/lcd_while/ultrasonic.c
@@ -0,0 +1,120 @@
+﻿#include "parking.h"
+
+char key;
+char keybuf[30];
+char buf[10];
+char input_password [4];
+char inital_password [4];
+char area_password [3][5];
+int i ;
+int k ;
+int m ;
+int num ;
+int num2 ;
+
+volatile int x;
+
+//초음파센서 변수
+volatile unsigned int  buf2[3],dist[3],start,end;
+volatile unsigned char cnt,flag[3];
+
+
+ISR(INT0_vect) // 에코 PD0
+{
+	if(EICRA==0x03)
+	start=TCNT3;
+	else
+	{
+		end=TCNT3;
+		buf2[0]=end-start;
+		EIMSK=0;
+		flag[0]=1;
+		if (dist[0]<0.5)
+		{
+			PORTF &= ~0x01;
+		}
+		else
+		{
+			PORTF |= 0x01;
+		}
+	}
+	EICRA^=0x01;
+}
+
+ISR(INT1_vect) // 에코 PD1
+{
+	if(EICRA==0x0C)
+	start=TCNT3;
+	else
+	{
+		end=TCNT3;
+		buf2[1]=end-start;
+		EIMSK=0;
+		flag[1]=1;
+		if (dist[1]<0.5)
+		{
+			PORTF &= ~0x02;
+		}
+		else
+		{
+			PORTF |= 0x02;
+		}
+	}
+	EICRA^=0x04;
+}
+ISR(INT2_vect)  // 에코 PD2
+{
+	if(EICRA==0x30)
+	start=TCNT3;
+	else
+	{
+		end=TCNT3;
+		buf2[2]=end-start;
+		EIMSK=0;
+		flag[2]=1;
+		if (dist[2]<0.5)
+		{
+			PORTF &= ~0x04;
+		}
+		else
+		{
+			PORTF |= 0x04;
+		}
+	}
+	EICRA^=0x10;
+}
+
+ISR(TIMER3_COMPA_vect)
+{
+	switch(cnt)
+	{
+		case 0:
+		PORTD|=0x10;
+		_delay_us(10);
+		PORTD&=~0x10;
+		EICRA=0x03; //인터럽트 0~3 설정
+		EIFR=0xFF;  // 인터럽트 클리어 해주는 레지스터
+		EIMSK=0x01; // 개별적 인터럽트 허용 레지스터
+		break;
+		
+		case 1:
+		PORTD|=0x20;
+		_delay_us(10);
+		PORTD&=~0x20;
+		EICRA=0x0C;
+		EIFR=0xFF;
+		EIMSK=0x02;
+		break;
+		
+		case 2:
+		PORTD|=0x40;
+		_delay_us(10);
+		PORTD&=~0x40;
+		EICRA=0x30;
+		EIFR=0xFF;
+		EIMSK=0x04;
+		break;
+	}
+	if(++cnt>3)
+	cnt=0;
+}
